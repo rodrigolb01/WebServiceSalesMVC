0958c1f524644bca30174d1d1082da0b1377df73
USER 010101 <rodrigolb01@gmail.com>
2021-03-12 18:13:47 -0300

    Deleting view and controllers of Departments

---
+++ WebApplication1/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+     }
+}                                           
+
