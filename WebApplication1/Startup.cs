b18b83a02a3ca2720bca5d806efa192f0bb0a417
USER 010101 <rodrigolb01@gmail.com>
2021-03-14 13:15:22 -0300

    CRUD Implementation using scaffolding and mySQL

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
+using MySql;
 
 namespace WebApplication1
 {
@@ -37,7 +38,8 @@ namespace WebApplication1
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebApplication1Context>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebApplication1Context")));
+                    options.UseMySql(Configuration.GetConnectionString("WebApplication1Context"), builder =>
+                    builder.MigrationsAssembly("WebApplication1")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
